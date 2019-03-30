56be40d6934c71c97296a43eb560179fb97cbaf9
Ufuk Tosun <tosunufuk@outlook.com>
2019-03-30 16:50:17 +0300

    Create problem_16.java

---
+++ problem_16.java
@@ -0,0 +1,29 @@
+import java.math.BigInteger;
+
+public class problem_16 {
+
+	public static void main(String[] args) {
+
+		int number = 2;
+
+		int sum = 0;
+
+		BigInteger integer = new BigInteger(String.valueOf(number));
+
+		BigInteger power = integer.pow(1000);
+
+		System.out.println(power);
+
+		String powerString = power.toString();
+
+		for (int i = 0; i < powerString.length(); i++) {
+
+			sum += Integer.parseInt("" + powerString.charAt(i));
+
+		}
+
+		System.out.println(sum);
+
+	}
+
+}
