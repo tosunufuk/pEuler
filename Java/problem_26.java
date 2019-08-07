86a1361f3b27d8d80b49f88c4586694586e64d2d
Ufuk Tosun <tosunufuk@outlook.com>
2019-08-07 15:36:10 +0300

    longest recurring cycle in decimal fraction part.
    value of d < 1000 for which 1/d contains the longest recurring cycle in its decimal fraction part.

---
+++ Java/problem_26.java
@@ -0,0 +1,57 @@
+import java.util.concurrent.TimeUnit;
+import java.util.ArrayList;
+
+public class Problem_26 {
+    
+    public static void main(String args[]) {
+        
+        long startTime = System.nanoTime();
+        
+        int dividend = 1;
+        
+        int index = 0;
+        
+        int maxDivisor = 1000;
+        
+        int theDivisor = 0;
+        
+        int theLength = 0;
+        
+        for(int divisor = 1; divisor <= maxDivisor; divisor++)
+        {
+            
+            ArrayList<Integer> list = new ArrayList<Integer>();
+            
+            dividend = 1;
+            
+            index = 0;
+            
+            while(!list.contains(dividend))
+            {
+                
+                list.add(dividend);
+                
+                dividend = dividend * 10 % divisor;
+                
+                index++;
+                
+            }
+            
+            if (index  > theLength)
+            {
+                
+                theDivisor = divisor;
+                
+                theLength = index;
+                
+            }
+            
+        }
+        
+        System.out.println("Divisor is " + theDivisor + " and length is " + theLength);
+        
+        System.out.println("Total time " + TimeUnit.NANOSECONDS.toSeconds(System.nanoTime() - startTime));
+        
+    }
+
+}
