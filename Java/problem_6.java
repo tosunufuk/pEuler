c9ccdd9dfa1875bb97590d067e47251d9026f002
tosunufuk <tosunufuk@outlook.com>
2019-08-19 19:25:10 +0300

    10 001st prime number
    removed Math.pow(double a, double b) added power(int a, int b) method instead

--- Java/problem_6.java
+++ Java/problem_6.java
@@ -1,39 +1,54 @@
 public class problem_6 {
-    
-    public static void main(String args[]) {
-        
-        int max = 100;
-        
-        int tmpSquareSum = 0;
-        
-        int squareSum = 0;
-        
-        int total = 0;
-        
-        int totalSquare = 0;
-        
-        int diff = 0;
-        
-        for (int i = 1; i <= max ; i++){
-            
-            tmpSquareSum = (int) Math.pow(i, 2);
-            
-            squareSum += tmpSquareSum;
-            
-            total += i;
-            
-        }
-        
-        totalSquare = (int) Math.pow(total, 2);
-        
-        diff= totalSquare - squareSum;
-        
-        System.out.println("squareSum " + squareSum);
-        
-        System.out.println("totalSquare " + totalSquare);
-        
-        System.out.println("diff " + diff);
-        
-    }
-    
+
+	public static void main(String args[]) {
+
+		int max = 100;
+
+		int tmpSquareSum = 0;
+
+		int squareSum = 0;
+
+		int total = 0;
+
+		int totalSquare = 0;
+
+		int diff = 0;
+
+		for (int i = 1; i <= max; i++) {
+
+			tmpSquareSum = power(i, 2);
+
+			squareSum += tmpSquareSum;
+
+			total += i;
+
+		}
+
+		totalSquare = power(total, 2);
+
+		diff = totalSquare - squareSum;
+
+		System.out.println("squareSum " + squareSum);
+
+		System.out.println("totalSquare " + totalSquare);
+
+		System.out.println("diff " + diff);
+
+	}
+
+	private static int power(int value, int powerOf) {
+
+		int powered = 1;
+
+		while (powerOf > 0) {
+
+			powered *= value;
+
+			powerOf--;
+
+		}
+
+		return powered;
+	}
+
 }
