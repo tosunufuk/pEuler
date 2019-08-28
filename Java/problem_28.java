22235d05dbbcef21b36c18c8734e68109a233415
tosunufuk <tosunufuk@outlook.com>
2019-08-28 20:33:19 +0300

    Number spiral diagonals
    sum of the numbers on the diagonals in a 1001 by 1001 spiral formed in the same way

---
+++ Java/problem_28.java
@@ -0,0 +1,43 @@
+package problem_28;
+
+import java.util.concurrent.TimeUnit;
+
+public class problem_28 {
+	
+	public static void main(String[] args) {
+		
+		long startTime = System.nanoTime();
+		
+		int spiralSize = 1001 / 2;
+		
+		int increment = 2;
+		
+		int spriralIndex = 1;
+		
+		int diagonal = 1;
+		
+		int sum = 1;
+		
+		while (spriralIndex <= spiralSize) {
+			
+			for (int i = 0; i < 4; i++) {
+				
+				diagonal = diagonal + increment;
+				
+				sum = sum + diagonal;
+				
+			}
+			
+			increment = increment + 2;
+			
+			spriralIndex++;
+			
+		}
+		
+		System.out.println(sum);
+		
+		System.out.println("Total time " + TimeUnit.NANOSECONDS.toSeconds(System.nanoTime() - startTime));
+		
+	}
+
+}
