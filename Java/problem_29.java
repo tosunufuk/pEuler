8560e184809f011a9e06170febbb62f089e8560f
tosunufuk <tosunufuk@outlook.com>
2019-08-28 21:37:21 +0300

    Distinct powers
    numbers of distinct terms in the sequence generated by ab for 2 ≤ a ≤ 100 and 2 ≤ b ≤ 100

---
+++ Java/problem_29.java
@@ -0,0 +1,35 @@
+package problem_29;
+
+import java.math.BigInteger;
+import java.util.HashSet;
+import java.util.concurrent.TimeUnit;
+
+public class problem_29 {
+
+	public static void main(String[] args) {
+		
+		long startTime = System.nanoTime();
+
+		HashSet<BigInteger> set = new HashSet<BigInteger>();
+
+		BigInteger val = BigInteger.valueOf(0);
+
+		for (int a = 2; a <= 100; a++) {
+
+			for (int b = 2; b <= 100; b++) {
+
+				val = BigInteger.valueOf(a).pow(b);
+
+				set.add(val);
+
+			}
+
+		}
+
+		System.out.println(set.size());
+		
+		System.out.println("Total time " + TimeUnit.NANOSECONDS.toSeconds(System.nanoTime() - startTime));
+
+	}
+
+}
