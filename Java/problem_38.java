936207f651314863c05530c69ccbadd934118760
tosunufuk <tosunufuk@outlook.com>
2019-10-20 15:32:05 +0300

    Pandigital multiples
    Largest 1 to 9 pandigital 9-digit number that can be formed as the concatenated product of an integer with (1,2, ... , n) where n > 1

---
+++ Java/problem_38.java
@@ -0,0 +1,32 @@
+package problem_38;
+
+import java.util.Arrays;
+import java.util.concurrent.TimeUnit;
+
+public class problem_38 {
+
+	public static void main(String[] args) {
+
+		long startTime = System.nanoTime();
+		
+		for (int i = 9387; i > 9234; i--) {
+			
+			char[] cs = ("" + i + (i * 2)).toCharArray();
+
+			Arrays.sort(cs);
+
+			if (new String(cs).equals("123456789")) {
+
+				System.out.println("" + i + (i * 2));
+
+				break;
+
+			}
+
+		}
+		
+		System.out.println("Total time " + TimeUnit.NANOSECONDS.toSeconds(System.nanoTime() - startTime));
+
+	}
+	
+}
