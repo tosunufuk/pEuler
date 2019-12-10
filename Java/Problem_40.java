fa051f6409c185a06be5c76bed81c6ca4d2961bd
tosunufuk <tosunufuk@outlook.com>
2019-12-10 19:25:11 +0300

    Champernowne's constant
    Champernowne's constant

---
+++ Java/Problem_40.java
@@ -0,0 +1,47 @@
+package problem_40;
+
+import java.util.ArrayList;
+
+public class Problem_40 {
+
+	private ArrayList<Character> characters = new ArrayList<Character>();
+
+	public static void main(String[] args) {
+
+		Problem_40 problem_40 = new Problem_40();
+
+		problem_40.solve();
+
+	}
+
+	private void solve() {
+
+		int value = 1;
+
+		String string = "";
+
+		for (int i = 1; characters.size() < 1000000; i++) {
+
+			string = "" + i;
+
+			for (int j = 0; j < string.length(); j++) {
+
+				characters.add(string.charAt(j));
+
+			}
+
+		}
+
+		value = Character.getNumericValue(characters.get(1 - 1)) 
+				* Character.getNumericValue(characters.get(10 - 1))
+				* Character.getNumericValue(characters.get(100 - 1))
+				* Character.getNumericValue(characters.get(1000 - 1))
+				* Character.getNumericValue(characters.get(10000 - 1))
+				* Character.getNumericValue(characters.get(100000 - 1))
+				* Character.getNumericValue(characters.get(1000000 - 1));
+
+		System.out.println(value);
+
+	}
+
+}
